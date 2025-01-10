dbae45ec029c371ebb8e3f27ace4d5e0fc6ca17a
Thibauld Braet <thibauld@live.be>
2025-01-10 16:57:03 +0100

    Create functions permutations

--- rosalind/combinatorics.py
+++ rosalind/combinatorics.py
@@ -1,3 +1,8 @@
+from itertools import permutations
+from math import factorial
+from typing import List, Tuple
+
+
 def fibonacci_rabbits(n, k=1):
     """
     Calculates the number of rabbit pairs after n months,
@@ -30,3 +35,40 @@ def fibonacci_rabbits(n, k=1):
         prev1 = current
 
     return prev1
+
+def get_number_of_permutations(n: int) -> int:
+    """
+    Calculate the total number of permutations for a given integer.
+
+    Args:
+        n (int): The integer for which to calculate the number of permutations.
+
+    Returns:
+        int: The total number of permutations.
+
+    Examples:
+        >>> get_number_of_permutations(3)
+        6
+    """
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    return factorial(n)
+
+
+def get_all_permutations(n: int) -> List[Tuple[int, ...]]:
+    """
+    Generate all possible permutations for numbers from 1 to n.
+
+    Args:
+        n (int): The integer for which to generate permutations.
+
+    Returns:
+        List[Tuple[int, ...]]: A list of all possible permutations.
+
+    Examples:
+        >>> get_all_permutations(3)
+        [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
+    """
+    if n < 1:
+        raise ValueError("Input must be a positive integer.")
+    return list(permutations(range(1, n + 1)))
